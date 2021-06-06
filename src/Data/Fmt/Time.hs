89c44200b267d3c4097c4f717fd30ba9526c8415
cmk <chris.mckinlay@gmail.com>
2021-06-06 00:57:58 -0700

    Add Html.hs

--- src/Data/Fmt/Time.hs
+++ src/Data/Fmt/Time.hs
@@ -1,131 +1,152 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Data.Fmt.Time (
-    fmtTime,
+    time,
+    date,
+    euro,
     -- Time
-    c,
-    f,
-    t,
-    ep,
-    ez,
-    z,
-    -- Time units
-    s,
-    m,
-    h,
-    p,
-    w,
-    d,
-    b,
-    y,
+    
+    -- Intra-day
+    hm,
+    hms,
+    second,
+    minute,
+    hour,
+    
+    -- Inter-day
+    day,
+    day',
+    month,
+    month',
+    year,
+    unix,
+    zone,
     -- Duration
-    ss,
-    mm,
-    hh,
-    dd,
-    yy,
+    secs,
+    mins,
+    hours,
+    days,
+    years,
 ) where
 
-import Data.Fmt.Type as Fmt ((%), fmt, fmt1, Fmt1, runFmt, IsString(..))
-import qualified Data.Fmt.Type as Fmt
+import Control.Arrow (arr)
+import Data.Fmt.Type as Fmt --((%), fmt, fmt1, Fmt, Fmt1, runFmt)
+--import qualified Data.Fmt.Type as Fmt
+import Data.String
 import Data.Time (FormatTime, formatTime, defaultTimeLocale)
+import Data.Time.Format.ISO8601
+import Prelude
 
+--iso8601 :: (IsString m, Monoid m, ISO8601 a) => Fmt m a m
+iso8601 :: ISO8601 a => Fmt LogStr a LogStr
+iso8601 = arr $ fromString . iso8601Show
 
 -- | A custom time formatter.
 --
 -- See "Data.Time.Format".
-fmtTime :: (IsString m, FormatTime a) => String -> Fmt1 m s a
-fmtTime s = fmt1 $ fromString . formatTime defaultTimeLocale s
+time :: (IsString m, FormatTime a) => String -> Fmt1 m s a
+time s = fmt1 $ fromString . formatTime defaultTimeLocale s
+
+-- | Local time: /05 Jun 2021 01:46:46 -0700/
+date :: (IsString m, FormatTime a) => Fmt1 m s a
+date = time "%d %b %Y %T %z"
+
+-- | Local time (Euro convention): /2021-06-05 01:46:46 -0700/
+euro :: (IsString m, FormatTime a) => Fmt1 m s a
+euro = time "%Y-%m-%d %T %z"
+
+-- now <- getZonedTime
+-- printf (time "%a %b %e %H:%M:%S %EZ %Y") now
+--
 
 -- Time
 
 -------------------------
 
--- | Local time as 'dateTimeFmt' @locale@ (e.g. @%a %b %e %H:%M:%S %Z %Y@).
-c :: (IsString m, FormatTime a) => Fmt1 m s a
-c = fmtTime "%c"
+-- | Time: @%m/%d/%y@.
+--d :: (IsString m, FormatTime a) => Fmt1 m s a
+--d = time "%D"
 
--- | Time: @%Y-%m-%d@.
-f :: (IsString m, FormatTime a) => Fmt1 m s a
-f = fmtTime "%F"
+-- | Time: @%Y/%m/%d@.
+--f :: (IsString m, FormatTime a) => Fmt1 m s a
+--f = time "%F"
+
+
+
 
 -- | Time: @%H:%M@.
-r :: (IsString m, FormatTime a) => Fmt1 m s a
-r = fmtTime "%R"
+hm :: (IsString m, FormatTime a) => Fmt1 m s a
+hm = time "%R"
 
 -- | Time: @%H:%M:%S@.
-t :: (IsString m, FormatTime a) => Fmt1 m s a
-t = fmtTime "%T"
+hms :: (IsString m, FormatTime a) => Fmt1 m s a
+hms = time "%T"
 
--- | Seconds from Unix epoch.
-ep :: (IsString m, FormatTime a) => Fmt1 m s a
-ep = fmtTime "%s"
-
--- | Timezone offset: @-HH:MM@.
-ez :: (IsString m, FormatTime a) => Fmt1 m s a
-ez = fmtTime "%Ez"
-
--- | Timezone offset: @-HHMM@.
-z :: (IsString m, FormatTime a) => Fmt1 m s a
-z = fmtTime "%z"
 
 -- Time units
 
 -------------------------
 
 -- | Second of minute: @00@ - @60@.
-s :: (IsString m, FormatTime a) => Fmt1 m s a
-s = fmtTime "%S"
+second :: (IsString m, FormatTime a) => Fmt1 m s a
+second = time "%S"
 
 -- | Minute of hour: @00@ - @59@.
-m :: (IsString m, FormatTime a) => Fmt1 m s a
-m = fmtTime "%M"
+minute :: (IsString m, FormatTime a) => Fmt1 m s a
+minute = time "%M"
 
 -- | Hour of day: @00@ - @23@.
-h :: (IsString m, FormatTime a) => Fmt1 m s a
-h = fmtTime "%H"
-
--- | Day half from ('amPm' @locale@), @AM@ - @PM@.
-p :: (IsString m, FormatTime a) => Fmt1 m s a
-p = fmtTime "%p"
-
--- | Day of week: @Sun@ - @Sat@.
-w :: (IsString m, FormatTime a) => Fmt1 m s a
-w = fmtTime "%a"
+hour :: (IsString m, FormatTime a) => Fmt1 m s a
+hour = time "%H"
 
 -- | Day of month: @01@ - @31@.
-d :: (IsString m, FormatTime a) => Fmt1 m s a
-d = fmtTime "%d"
+day :: (IsString m, FormatTime a) => Fmt1 m s a
+day = time "%d"
+
+-- | Day of week: @Sun@ - @Sat@.
+day' :: (IsString m, FormatTime a) => Fmt1 m s a
+day' = time "%a"
+
+-- | Month of year: @01@ - @12@.
+month :: (IsString m, FormatTime a) => Fmt1 m s a
+month = time "%m"
 
 -- | Month of year: @Jan@ - @Dec@.
-b :: (IsString m, FormatTime a) => Fmt1 m s a
-b = fmtTime "%b"
+month' :: (IsString m, FormatTime a) => Fmt1 m s a
+month' = time "%b"
 
 -- | Year.
-y :: (IsString m, FormatTime a) => Fmt1 m s a
-y = fmtTime "%Y"
+year :: (IsString m, FormatTime a) => Fmt1 m s a
+year = time "%Y"
 
+-- | Seconds from Unix epoch.
+unix :: (IsString m, FormatTime a) => Fmt1 m s a
+unix = time "%s"
+
+-- | Timezone offset: @-HHMM@.
+zone :: (IsString m, FormatTime a) => Fmt1 m s a
+zone = time "%z"
 
 -- Duration
 
 -------------------------
 
 -- | Display to a given precision the absolute value time span in seconds.
-ss :: IsString m => Int -> Fmt1 m r Double
-ss n = fmt1 (runFmt (Fmt.f n) . abs . count) where count n' = n'
+secs :: IsString m => Int -> Fmt1 m s Double
+secs n = fmt1 (runFmt (Fmt.f n) . abs . count) where count n' = n'
 
 -- | Display to a given precision the absolute value time span in minutes.
-mm :: IsString m => Int -> Fmt1 m r Double
-mm n = fmt1 (runFmt (Fmt.f n) . abs . count) where count n' = n' / 60
+mins :: IsString m => Int -> Fmt1 m s Double
+mins n = fmt1 (runFmt (Fmt.f n) . abs . count) where count n' = n' / 60
 
 -- | Display to a given precision the absolute value time span in hours.
-hh :: IsString m => Int -> Fmt1 m r Double
-hh n = fmt1 (runFmt (Fmt.f n) . abs . count) where count n' = n' / 60 / 60
+hours :: IsString m => Int -> Fmt1 m s Double
+hours n = fmt1 (runFmt (Fmt.f n) . abs . count) where count n' = n' / 60 / 60
 
 -- | Display to a given precision the absolute value time span in days.
-dd :: (IsString m) => Int -> Fmt1 m r Double
-dd n = fmt1 (runFmt (Fmt.f n) . abs . count) where count n' = n' / 24 / 60 / 60
+days :: (IsString m) => Int -> Fmt1 m s Double
+days n = fmt1 (runFmt (Fmt.f n) . abs . count) where count n' = n' / 24 / 60 / 60
 
 -- | Display to a given precision the absolute value time span in years.
-yy :: IsString m => Int -> Fmt1 m r Double
-yy n = fmt1 (runFmt (Fmt.f n) . abs . count) where count n' = n' / 365 / 24 / 60 / 60
+years :: IsString m => Int -> Fmt1 m s Double
+years n = fmt1 (runFmt (Fmt.f n) . abs . count) where count n' = n' / 365 / 24 / 60 / 60
 
